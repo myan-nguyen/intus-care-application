f6a9362c76a82d47a248c925761909447d8da53b
myan-nguyen <myan_nguyen@brown.edu>
2025-03-03 16:59:05 -0500

    use env variable for api base url

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Participant } from '../types/types';
 
-const API_BASE_URL = 'http://localhost:5002';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5002';
 
 export const fetchParticipants = async (): Promise<Participant[]> => {
   const response = await axios.get(`${API_BASE_URL}/participants`);
