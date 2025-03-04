47346fee0f56a6d64d201893cc91c0be8a599469
myan-nguyen <myan_nguyen@brown.edu>
2025-03-03 19:38:30 -0500

    bugs

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,4 +1,4 @@
-/*mport axios from 'axios';
+import axios from 'axios';
 import { Participant } from '../types/types';
 
 // dynamically set the API base URL based on the environment (local vs production)
@@ -14,9 +14,9 @@ export const fetchParticipants = async (): Promise<Participant[]> => {
 export const fetchParticipantById = async (id: string): Promise<Participant> => {
   const response = await axios.get(`${API_BASE_URL}/participants/${id}`);
   return response.data;
-};*/
+};
 
-import axios from 'axios';
+/*import axios from 'axios';
 import { Participant } from '../types/types';
 
 // Dynamically set the API base URL based on the environment (local vs production)
@@ -32,4 +32,4 @@ export const fetchParticipants = async (): Promise<Participant[]> => {
 export const fetchParticipantById = async (id: string): Promise<Participant> => {
   const response = await axios.get(`${API_BASE_URL}${process.env.NODE_ENV === 'production' ? '/api' : ''}/participants/${id}`);
   return response.data;
-};
+};*/
